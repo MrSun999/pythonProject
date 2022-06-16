4e26a2abd83c6521b13533e2e725ef25961cf48e
MrSun <a161401@163.com>
2022-06-16 20:02:56 +0800

    fixd

--- common/commonality.py
+++ common/commonality.py
@@ -14,3 +14,9 @@ class commonality():
             rdms.append(random.randint(111,9999))
         rds = list(set(rdms))[:num]
         return rds
+
+    def iterate_custom(self,list_of_items, custom_func):
+        self.list_of_items = list_of_items
+        self.custom_func = custom_func
+        for item in list_of_items:
+            custom_func(item)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -- ** -- UTF-8
 from common.commonality import commonality
 
+pass
+
 
 if __name__ == '__main__':
     tc = commonality()
